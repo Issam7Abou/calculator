619bc2c29569b0f572e4a47952bb2f36c73c958c
Issam Abou <113906543+Issam7Abou@users.noreply.github.com>
2023-04-17 14:16:58 +0100

    Add files via upload
    finished math logic;
    added mobile responsiveness;

--- script.js
+++ script.js
@@ -8,27 +8,53 @@ let operatorValue = '';
 let awaitingNextValue = false;
 
 function sendNumberValue(number) {
-    // If current display is 0, replace it, if not add number
-    const displayValue = calculatorDisplay.textContent
-    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    // Replace current display value if first value is entered
+    if (awaitingNextValue) {
+        calculatorDisplay.textContent = number;
+        awaitingNextValue = false;
+    } else {
+      // If current display is 0, replace it, if not add number
+      const displayValue = calculatorDisplay.textContent
+      calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
 }
 
 function addDecimal() {
+    // If operator pressed, don't add decimal
+    if (awaitingNextValue) return;
     //If no decimal, add one
     if (!calculatorDisplay.textContent.includes('.')) {
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
     }
 }
 
+// Calculate first and second value depending on operator
+const calculate = {
+    '/': (firstnNumber, secondNumber) => firstnNumber / secondNumber,
+    '*': (firstnNumber, secondNumber) => firstnNumber * secondNumber,
+    '+': (firstnNumber, secondNumber) => firstnNumber + secondNumber,
+    '-': (firstnNumber, secondNumber) => firstnNumber - secondNumber,
+    '=': (firstnNumber, secondNumber) => secondNumber,
+};
+
 function useOperator(operator) {
+    // Prevent multiple operators
+    if (operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return
+    };
     const currentValue = Number(calculatorDisplay.textContent);
     // Assign firstValue if no value
     if (!firstValue) {
         firstValue = currentValue;
+    } else {
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;
     }
+    // Ready for next value, store operator
+    awaitingNextValue = true;
     operatorValue = operator;
-    console.log('firstValue', firstValue);
-    console.log('operatorValue', operatorValue);
 }
 
 // Event Listeners for numbers, operators, decimal buttons
